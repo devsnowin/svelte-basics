0514a151bdd9503b6ed81e7007fb221dd1174a1b
devsnowin <20103104@hit.edu.in>
2022-07-26 16:07:19 +0530

    start learn svelte

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<main>
+	<h1>Let's learn svelte 🚀</h1>
+</main>
+
+<style>
+	:global(*) {
+		margin: 0 !important;
+		padding: 0 !important;
+		box-sizing: border-box !important;
+	}
+
+	main {
+		max-width: 60rem;
+		min-height: 100vh;
+		margin: 0 auto !important;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
